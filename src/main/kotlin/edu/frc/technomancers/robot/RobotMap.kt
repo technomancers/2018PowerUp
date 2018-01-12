667ac327e0aaec6ecc9d7ddb9504087d385cb125
the15thdoctorwho <cam1carsten1@gmail.com>
2018-01-11 19:56:39 -0500

    First implementation of Network Tables

--- src/main/kotlin/edu/frc/technomancers/robot/RobotMap.kt
+++ src/main/kotlin/edu/frc/technomancers/robot/RobotMap.kt
@@ -1,4 +1,21 @@
 package edu.frc.technomancers.robot
 
+import edu.wpi.first.networktables.NetworkTableInstance
+
 object RobotMap {
+    val ntTable = NetworkTableInstance.getDefault()
+    val table = ntTable.getTable("Configuration")
+
+    private val frontLeftMag = table.getEntry("frontLeftMag")
+    val FRONT_LEFT_MAG : Number
+
+    init {
+        frontLeftMag.setPersistent()
+
+        if (!frontLeftMag.exists()){
+            frontLeftMag.setNumber(0)
+        }
+
+        FRONT_LEFT_MAG = frontLeftMag.getNumber(0)
+    }
 }
