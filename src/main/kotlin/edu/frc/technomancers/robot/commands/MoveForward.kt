abcb6e57adef2b936243b8a13db389a359982f8e
Deep Patel <deepkalpana1@aol.com>
2018-01-29 18:54:20 -0500

    MoveForward

---
+++ src/main/kotlin/edu/frc/technomancers/robot/commands/MoveForward.kt
@@ -0,0 +1,9 @@
+package edu.frc.technomancers.robot.commands
+
+import edu.frc.technomancers.robot.Operator
+
+class MoveForward : CommandBase(){
+    override fun isFinished(): Boolean {
+
+    }
+}
