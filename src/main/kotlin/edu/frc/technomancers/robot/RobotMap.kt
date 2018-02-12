09d8fe72182744a05bec0a8080afd35edda22ad7
Etash Guha <EtashGuha>
2018-02-11 16:38:17 -0800

    Cleaned up Robot Map

--- src/main/kotlin/edu/frc/technomancers/robot/RobotMap.kt
+++ src/main/kotlin/edu/frc/technomancers/robot/RobotMap.kt
@@ -20,22 +20,6 @@ object RobotMap {
     val ROBOT_LENGTH: Double
 
     val ENCODER_TICKS_PER_REVOLUTION: Int
-
-    val FORWARD_CHANNEL: Int
-    val REVERSE_CHANNEL: Int
-
-    val ULTRASONIC_VOLTAGE : Double
-    val VOLTS_PER_INCH : Double
-
-    val COMPRESSOR_NODE_ID: Int
-
-    //Ultrasonics
-    val FRONT_LEFT_SONIC: Int
-    val FRONT_RIGHT_SONIC: Int
-    val RIGHT_SONIC: Int
-    val BACK_SONIC: Int
-    val LEFT_SONIC: Int
-
     init {
         FRONT_LEFT_MOTOR_DIRECTIONAL = pref.getInt("frontLeftDir", 0)
         FRONT_LEFT_MOTOR_ROTATIONAL = pref.getInt("frontLeftRot", 0)
@@ -53,21 +37,5 @@ object RobotMap {
         ROBOT_WIDTH = pref.getDouble("RobotWidth", 0.0)
 
         ENCODER_TICKS_PER_REVOLUTION = pref.getInt("encoderTicksPerRevolution", 0)
-
-        FORWARD_CHANNEL = pref.getInt("forwardChannel", 0)
-        REVERSE_CHANNEL = pref.getInt("reverseChannel", 0)
-
-        FRONT_LEFT_SONIC = pref.getInt("frontLeftSonic", 0)
-        FRONT_RIGHT_SONIC = pref.getInt("frontRightSonic", 0)
-        LEFT_SONIC = pref.getInt("leftSonic", 0)
-        RIGHT_SONIC = pref.getInt("rightSonic", 0)
-        BACK_SONIC = pref.getInt("backSonic", 0)
-
-        ULTRASONIC_VOLTAGE = pref.getDouble("ultrasonicVoltage", 0.0)
-        VOLTS_PER_INCH = ULTRASONIC_VOLTAGE / 512
-
-        COMPRESSOR_NODE_ID = pref.getInt("CompressorNodeID", 0)
-
-        
     }
 }
