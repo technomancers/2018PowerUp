5b6cfbbcee6c4912283829c08be6d8b060c351b7
Etash Guha <EtashGuha>
2018-02-11 16:09:37 -0800

    fixed RobotMap delarations

--- src/main/kotlin/edu/frc/technomancers/robot/RobotMap.kt
+++ src/main/kotlin/edu/frc/technomancers/robot/RobotMap.kt
@@ -10,13 +10,13 @@ object RobotMap {
     val FRONT_RIGHT_MOTOR_DIRECTIONAL: Int
     val FRONT_RIGHT_MOTOR_ROTATIONAL: Int
 
-    val BACK_LEFT_MOTOR_DIRECTIONAL = Int
+    val BACK_LEFT_MOTOR_DIRECTIONAL: Int
     val BACK_LEFT_MOTOR_ROTATIONAL: Int
 
     val BACK_RIGHT_MOTOR_DIRECTIONAL: Int
     val BACK_RIGHT_MOTOR_ROTATIONAL: Int
 
-    val ROBOT_LIFT = Int
+    val ROBOT_LIFT: Int
 
     val ROBOT_WIDTH: Double
     val ROBOT_LENGTH: Double
@@ -30,13 +30,13 @@ object RobotMap {
         FRONT_RIGHT_MOTOR_DIRECTIONAL = pref.getInt("frontRightDir", 0)
         FRONT_RIGHT_MOTOR_ROTATIONAL = pref.getInt("frontRightRot", 0)
 
-        //BACK_LEFT_MOTOR_DIRECTIONAL = pref.getInt("backLeftDir", 0)
+        BACK_LEFT_MOTOR_DIRECTIONAL = pref.getInt("backLeftDir", 0)
         BACK_LEFT_MOTOR_ROTATIONAL = pref.getInt("backLeftRot", 0)
 
         BACK_RIGHT_MOTOR_DIRECTIONAL = pref.getInt("backRightDir", 0)
         BACK_RIGHT_MOTOR_ROTATIONAL = pref.getInt("backRightRot", 0)
 
-        //ROBOT_LIFT = pref.getInt("robotLift",0)
+        ROBOT_LIFT = pref.getInt("robotLift",0)
 
         ROBOT_LENGTH = pref.getDouble("robotLength", 0.0)
         ROBOT_WIDTH = pref.getDouble("RobotWidth", 0.0)
