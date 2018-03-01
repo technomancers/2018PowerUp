e2078d004d0f43bb83c36da6dc80c510dbbeb349
Etash Guha <EtashGuha>
2018-03-01 11:32:53 -0800

    PID Loop

--- src/main/kotlin/edu/frc/technomancers/utilities/WheelDrive.kt
+++ src/main/kotlin/edu/frc/technomancers/utilities/WheelDrive.kt
@@ -4,38 +4,45 @@ import com.ctre.phoenix.motorcontrol.ControlMode
 import com.ctre.phoenix.motorcontrol.can.TalonSRX
 import edu.frc.technomancers.robot.RobotMap
 import edu.wpi.first.wpilibj.AnalogInput
+import edu.wpi.first.wpilibj.PIDController
+import edu.wpi.first.wpilibj.PIDOutput
 import org.apache.commons.math3.util.FastMath
 import kotlin.math.sign
 
-class WheelDrive(speedMotorPort: Int, angleMotorPort: Int, analogPos: Int)
-{
+class WheelDrive(speedMotorPort: Int, angleMotorPort: Int, analogPos: Int) {
     private val speedMotor = TalonSRX(speedMotorPort)
     private val angleMotor = TalonSRX(angleMotorPort)
     private val encoder = AnalogInput(analogPos)
+    val pid = PIDController(1.0, 0.0, 0.0, encoder, PIDOutput{angleMotor})
+
+    init {
+        pid.enable()
+    }
 
     fun drive(speed: Double, angle: Double) {
         @Suppress("MagicNumber")
-        val current = encoder.voltage * RobotMap.ENCODER_TICKS_PER_REVOLUTION / 5.0
-        var speedOfWheel = speed
-        val midRelative = (FastMath.abs(current) % RobotMap.ENCODER_TICKS_PER_REVOLUTION) * current.sign
-        val lowRelative = midRelative - RobotMap.ENCODER_TICKS_PER_REVOLUTION
-        val highRelative = midRelative + RobotMap.ENCODER_TICKS_PER_REVOLUTION
-        var target = (RobotMap.ENCODER_TICKS_PER_REVOLUTION/2) * angle
-        val lowDelta = target - lowRelative
-        val midDelta = target - midRelative
-        val highDelta = target - highRelative
-        var delta = displacementMin(doubleArrayOf(lowDelta, midDelta, highDelta))
-        if(delta < RobotMap.ENCODER_TICKS_PER_REVOLUTION/-4){
-            delta += RobotMap.ENCODER_TICKS_PER_REVOLUTION/2
-            speedOfWheel *= -1
-        }
-        if(delta > RobotMap.ENCODER_TICKS_PER_REVOLUTION/4){
-            delta -= RobotMap.ENCODER_TICKS_PER_REVOLUTION/2
-            speedOfWheel *= -1
-        }
-        val next = delta + current
-        angleMotor.set(ControlMode.Position, next)
-        speedMotor.set(ControlMode.PercentOutput, speedOfWheel)
+//        val current = encoder.voltage * RobotMap.ENCODER_TICKS_PER_REVOLUTION / 5.0
+//        var speedOfWheel = speed
+//        val midRelative = (FastMath.abs(current) % RobotMap.ENCODER_TICKS_PER_REVOLUTION) * current.sign
+//        val lowRelative = midRelative - RobotMap.ENCODER_TICKS_PER_REVOLUTION
+//        val highRelative = midRelative + RobotMap.ENCODER_TICKS_PER_REVOLUTION
+//        var target = (RobotMap.ENCODER_TICKS_PER_REVOLUTION/2) * angle
+//        val lowDelta = target - lowRelative
+//        val midDelta = target - midRelative
+//        val highDelta = target - highRelative
+//        var delta = displacementMin(doubleArrayOf(lowDelta, midDelta, highDelta))
+//        if(delta < RobotMap.ENCODER_TICKS_PER_REVOLUTION/-4){
+//            delta += RobotMap.ENCODER_TICKS_PER_REVOLUTION/2
+//            speedOfWheel *= -1
+//        }
+//        if(delta > RobotMap.ENCODER_TICKS_PER_REVOLUTION/4){
+//            delta -= RobotMap.ENCODER_TICKS_PER_REVOLUTION/2
+//            speedOfWheel *= -1
+//        }
+//        val next = delta + current
+//        angleMotor.set(ControlMode.Position, next)
+//        speedMotor.set(ControlMode.PercentOutput, speedOfWheel)
+        deepsAlgorithm(angle, speed)
     }
 
     private fun displacementMin(deltas : DoubleArray):Double{
@@ -91,11 +98,7 @@ class WheelDrive(speedMotorPort: Int, angleMotorPort: Int, analogPos: Int)
             delta -= RobotMap.ENCODER_TICKS_PER_REVOLUTION/2.0
             finalSpeed *= -1.0
         }
-        if(delta > RobotMap.SWERVE_TOLERANCE) {
-            angleMotor.set(ControlMode.PercentOutput,-1.0)
-        } else if(delta < RobotMap.SWERVE_TOLERANCE){
-            angleMotor.set(ControlMode.PercentOutput,1.0)
-        }
+        pid.setpoint = delta + current
         speedMotor.set(ControlMode.PercentOutput, finalSpeed)
     }
 }
