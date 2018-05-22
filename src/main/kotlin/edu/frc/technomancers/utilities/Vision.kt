abb9854b8ce7bf6b76efd405609ca9d08572884a
EtashGuha <EtashGuha>
2018-05-21 19:38:12 -0700

    Prep for showoff

--- src/main/kotlin/edu/frc/technomancers/utilities/Vision.kt
+++ src/main/kotlin/edu/frc/technomancers/utilities/Vision.kt
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.CameraServer
 class Vision{
     init {
         val camera = CameraServer.getInstance().startAutomaticCapture()
+        @Suppress("MagicNumber")
         camera.setFPS(30)
         camera.setResolution(160, 120)
     }
